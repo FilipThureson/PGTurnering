d3bbc5613bff7324e0960e87354c65a08f0ee386
Filip <filip.tthuresson@gmail.com>
2021-02-23 22:24:50 +0100

    Started With Files

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+var elo = document.getElementById('elo');
+
+var allplayers = [];
+addBTN.addEventListener('click',()=>{
+    let name = document.getElementById('name').value;
+    let elo = document.getElementById('elo').value;
+});
