ccdbc80223622f924c2ed651dafcc38219ebfcee
Filip <filip.tthuresson@gmail.com>
2021-02-24 17:56:40 +0100

    Add Players
    Can now add players and they are stored in localstorage

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
+var addBTN = document.getElementById('addBTN');
+var playerOutput = document.getElementById('playerOutput');
+var allplayers = JSON.parse(localStorage.getItem("players")) ?? [];
 
-var elo = document.getElementById('elo');
-
-var allplayers = [];
+if(allplayers != null){
+    allplayers.forEach(player=> {
+        playerOutput.innerHTML += `<div><h3>${player.name}:${player.elo}</h3></div>`;
+    });
+}
 addBTN.addEventListener('click',()=>{
+
     let name = document.getElementById('name').value;
     let elo = document.getElementById('elo').value;
+
+    if(allplayers.includes(name)){
+        alert("Already Exists");
+    }else{
+        allplayers.push({name,elo});
+        localStorage.setItem("players", JSON.stringify(allplayers));
+        playerOutput.innerHTML += `<div><h3>${name}:${elo}</h3></div>`;
+        console.log(allplayers)
+    }
 });
